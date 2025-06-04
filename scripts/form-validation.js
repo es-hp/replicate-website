f6f013fe0c2cf83703e189ed457c350a6310d7dd
Helen <es.helenpark@gmail.com>
2025-06-04 12:44:26 -0400

    Added login.html, login.css, form-validation.js

---
+++ scripts/form-validation.js
@@ -0,0 +1,20 @@
+/* Form Validation from Bootstrap */
+(() => {
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
